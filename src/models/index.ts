4036d0241c65b6d6786e7ab7713e2b47ebcbd571
choon <chachooon@gmail.com>
2017-11-05 14:56:44 +0900

    1차 세미나 내용(사원/ 부서 모델링, 테스트코드) 추가

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,22 +1,23 @@
 import {Sequelize} from "sequelize-typescript";
 import DataSource from "../config/data-source";
 import Employee from "./domain/employee";
+import Team from './domain/team';
 
 class DatabaseConfig {
 
-	private _sequelize: Sequelize;
+  private _sequelize: Sequelize;
 
-	constructor() {
-		const sequelize = new Sequelize({
-			...new DataSource().getConfig
-		});
-		sequelize.addModels([Employee]);
-		this._sequelize = sequelize;
-	}
+  constructor() {
+    const sequelize = new Sequelize({
+      ...new DataSource().getConfig
+    });
+    sequelize.addModels([Employee, Team]);
+    this._sequelize = sequelize;
+  }
 
-	get getSequelize() {
-		return this._sequelize;
-	}
+  get getSequelize() {
+    return this._sequelize;
+  }
 }
 
 const database = new DatabaseConfig();
