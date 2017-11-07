7c54448a5a24775edb922a25677dc872b5227195
choon <chachooon@gmail.com>
2017-11-07 11:33:38 +0900

    게시글 등록 작성자 컬럼 추가 테스트

--- test/spec/models/post.integration.ts
+++ test/spec/models/post.integration.ts
@@ -1,7 +1,6 @@
 import {expect} from "chai";
 import {sequelize} from "../../../src/models/index";
 import Employee from "../../../src/models/domain/employee";
-// import Team from '../../../src/models/domain/team';
 import Post from '../../../src/models/domain/post';
 import Comment from '../../../src/models/domain/comment';
 
@@ -14,14 +13,20 @@ describe("[Integration] 게시글 모델을 테스트 한다", () => {
     });
   });
 
-  const cleanUp = (cb) => Post.destroy({where: {}, truncate: true}).then(() => cb());
+  const cleanUpEmployee = (cb) => Employee.destroy({where: {}, truncate: true}).then(() => cb());
+  const cleanUpPost = (cb) => Post.destroy({where: {}, truncate: true})
+    .then(() => Comment.destroy({where: {}, truncate: true}).then(() => cb()));
 
   beforeEach((done: Function) => {
-    cleanUp(() => done());
+    cleanUpPost(() => done());
   });
 
 
-  const save = (given, cb) => {
+  after((done:Function)=>{
+    cleanUpEmployee(() => done());
+  })
+
+  const savePost = (given, cb) => {
     const post = new Post(given);
     post.save()
       .then((createPost: Post) => {
@@ -29,23 +34,37 @@ describe("[Integration] 게시글 모델을 테스트 한다", () => {
       });
   };
 
-  Employee.create({name: 'test', address: 'JeJu'}).then(function(user) {
-    console.log(user) // user object with username as Boss and accessLevel of 20
-  })
-
+  it('테스트용 직원을 만든다', (done: Function) => {
+    // given
+    let employee = new Employee({name: 'test', address: 'jeju'});
+    // when & then
+    employee.save()
+      .then((createEmployee: Employee) => {
+        Employee.findOne<Employee>({where: {name: 'test'}})
+          .then((employee: Employee) => {
+            expect(employee.name).to.be.eql(createEmployee.name);
+            done();
+          });
+      });
+  });
 
   it('게시글을 등록할 때 등록한 값이 리턴된다', (done: Function) => {
-
     // given
-    let givenPost = {title: '게시글 테스트.', content: '게시글을 등록합니다.',};
+    Employee.findOne<Employee>({where: {name: 'test'}})
+      .then((employee: Employee) => {
+        let givenPost = {title: '게시글 테스트.', content: '게시글을 등록합니다.', userId:employee.id};
+
+        savePost(givenPost, (savedPost: Post) => {
+
+          done();
+        });
+
+        // done();
+      });
+
 
     // when
-    save(givenPost, (savePost: Post) => {
-      // then
-      expect(savePost.title).to.be.eql(givenPost.title);
-      expect(savePost.content).to.be.eql(givenPost.content);
-      done();
-    });
+
   });
 
 
