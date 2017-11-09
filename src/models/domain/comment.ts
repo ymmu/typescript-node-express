b8535280da0902c163e9c14201a199bd18de8d29
choon <chachooon@gmail.com>
2017-11-09 18:58:05 +0900

    대댓글 관계 수정

--- src/models/domain/comment.ts
+++ src/models/domain/comment.ts
@@ -1,6 +1,8 @@
-import {Table, Column, Model, CreatedAt, UpdatedAt, AllowNull, ForeignKey,HasMany} from 'sequelize-typescript';
+///<reference path="../../../node_modules/sequelize-typescript/lib/services/association.d.ts"/>
+import {Table, Column, Model, CreatedAt, UpdatedAt, AllowNull, ForeignKey,HasMany,BelongsTo} from 'sequelize-typescript';
 import Employee from './employee';
 import Post from './post';
+import {BELONGS_TO} from "sequelize-typescript/lib/services/association";
 
 @Table
 export default class Comment extends Model<Comment> {
@@ -22,7 +24,8 @@ export default class Comment extends Model<Comment> {
   @Column
   postId: number;
 
-  @ForeignKey(() => Comment)
+
+  @BelongsTo(() => Comment)
   @Column
   parentId: number;
 
